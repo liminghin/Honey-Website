67ac7c272a9137310d59ce38ac46e461743fd905
liminghin <machinelearninglesson@gmail.com>
2024-04-01 20:15:48 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,197 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="'viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bee Honey</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<!-- Honey Top Section -->
+
+<body>
+    <nav>
+
+        <div class="bg-amber-300 flex justify-between px-10 h-12 items-center">
+
+            <a href="" class="font-bold text-white text-2xl">Bee Honey</a>
+            <ul class="flex gap-4">
+                <li><a href="" class="font-semibold bg-amber-500 text-white hover:bg-amber-700 hover:text-white px-2 py-1 rounded">Home</a></li>
+                <li><a href="" class="font-semibold bg-amber-500 text-white hover:bg-amber-700 hover:text-white px-2 py-1 rounded">Honey Products</a></li>
+                <li><a href="" class="font-semibold bg-orange-500 text-white hover:bg-orange-700 hover:text-white px-2 py-1 rounded">Log In</a></li>
+                <li><a href="" class="font-semibold bg-orange-500 text-white hover:bg-orange-700 hover:text-white px-2 py-1 rounded">Sign In</a></li>
+
+            </ul>
+        </div>
+
+    </nav>
+
+<!-- Honey Top Page Screen -->
+
+    <section class="h-64 w-full bg-yellow-200 flex items-center justify-center" style="background-image: url('{{ asset('img/yellow-hexagonal-honeycomb-mesh-pattern-with-text-space_1017-26292_jpg.png') }}'); background-size: cover; background-size: cover;">
+
+        <div class="text-center">
+            <h1 class="font-bold text-5xl text-center mb-4 text-white shadow-lg">Bee Honey</h1>
+            <p class="font-semibold text-white shadow-lg">This Website is about honey products</p>
+        </div>
+
+    </section>
+
+<!-- What makes a honey special -->
+
+    <section class="h-auto w- p-4 flex justify-center gap-2 bg-yellow-300">
+        <div class="text-center">
+            <h1 class="font-semibold text-2xl text-center text-white mb-4">What makes a honey special?</h1>
+            <p class="font-semibold text-white text-center">A honey makes special for some following facts:</p>
+        </div>
+    </section>
+
+    <section class="h-auto w-full p-4 flex justify-center gap-2 bg-yellow-500">
+        
+
+
+        <div class="h-auto w-52 sm:w-52 shadow-md rounded bg-yellow-200 outline outline-5 outline-yellow-300">
+            <div class="flex flex-col justify-center items-center h-25">
+                <h1 class="font-semibold text-xl text-yellow-500 text-center">It is rich in a mix of vitamins, minerals and nutrients</h1>
+                <p class="text-center p-3"></p>
+             </div>
+        </div>
+
+        <div class="h-auto w-52 sm:w-52 shadow-md rounded bg-yellow-200 outline outline-5 outline-yellow-300">
+            <div class="flex flex-col justify-center items-center h-25">
+                <h1 class="font-semibold text-xl text-yellow-500 text-center">It has thicknesses in every drop</h1>
+                <p class="text-center p-3"></p>
+             </div>
+        </div>
+
+        <div class="h-auto w-52 sm:w-52 shadow-md rounded bg-yellow-200 outline outline-5 outline-yellow-300">
+            <div class="flex flex-col justify-center items-center h-25">
+                <h1 class="font-semibold text-xl text-yellow-500 text-center">Fresh, sparkling, and healthy in digestion</h1>
+                <p class="text-center p-3"></p>
+             </div>
+        </div>
+
+        <div class="h-auto w-52 sm:w-52 shadow-md rounded bg-yellow-200 outline outline-5 outline-yellow-300">
+            <div class="flex flex-col justify-center items-center h-25">
+                <h1 class="font-semibold text-xl text-yellow-500 text-center">No need for the sugar addition and is healthy for curing wounds and injuries</h1>
+                <p class="text-center p-3"></p>
+             </div>
+        </div>
+
+
+    </section>
+
+<!-- Honey Products -->
+
+    <section class="h-auto w- p-4 flex justify-center gap-2 bg-yellow-500">
+        
+        <div class="text-center">
+            <h1 class="font-semibold text-2xl text-center text-white mb-4">Honey Products</h1>
+        </div>
+
+    </section>
+
+    <section class="h-auto w-full p-4 flex justify-center gap-2 bg-yellow-600">
+        
+        <ul class="flex gap-4">
+            
+        <div class="h-auto w-52 sm:w-52 shadow-md rounded bg-orange-300 outline outline-5 outline-orange-700">
+            
+            <div class="h-1/2 overflow-hidden">
+                <img src="Img/longan-3d-icon-png.png" alt=""/>
+            </div>
+            
+            <div class="flex flex-col justify-center items-center h-25">
+                <h1 class="font-semibold text-xl text-orange-700">Longan Honey</h1>
+                <p class="text-center p-3"></p>
+                <a href="" class="font-semibold bg-orange-700 text-white hover:bg-orange-900 hover:text-white px-2 py-1 rounded pt-2 pb-2">Info</a>
+             </div>
+
+        </div>
+
+        <div class="h-auto w-52 sm:w-52 shadow-md rounded bg-amber-200 outline outline-5 outline-amber-500">
+            
+            <div class="h-1/2 overflow-hidden">
+                <img src="Img/Acacia_aulacocarpa_foliage_and_flowers.jpg" alt=""/>
+            </div>
+            
+            <div class="flex flex-col justify-center items-center h-25">
+                <h1 class="font-semibold text-xl text-amber-500">Acacia Carpa Honey</h1>
+                <p class="text-center p-3"></p>
+                <a href="" class="font-semibold bg-amber-500 text-white hover:bg-amber-700 hover:text-white px-2 py-1 rounded pt-2 pb-2">Info</a>
+             </div>
+
+        </div>
+
+        </ul>
+
+    </section>
+
+    <section class="h-auto w-full p-4 flex justify-center gap-2 bg-yellow-600">
+        <ul class="flex gap-4">
+        
+            <div class="h-auto w-52 sm:w-52 shadow-md rounded bg-yellow-100 outline outline-5 outline-yellow-300">
+            
+            <div class="h-1/2 overflow-hidden">
+                <img src="Img/Multiflora Flowers Transparent.png" alt=""/>
+            </div>
+
+            <div class="flex flex-col justify-center items-center h-25">
+                <h1 class="font-semibold text-xl text-yellow-300">Multifloral Honey</h1>
+                <p class="text-center p-3"></p>
+                <a href="" class="font-semibold bg-yellow-300 text-white hover:bg-yellow-500 hover:text-white px-2 py-1 rounded pt-2 pb-2">Info</a>
+             </div>
+
+        </div>
+
+        <div class="h-auto w-52 sm:w-52 shadow-md rounded bg-yellow-200 outline outline-5 outline-yellow-500">
+            
+            <div class="h-1/2 overflow-hidden">
+                <img src="Img/red-firework-flower-exotic-calliandra-plant-blossom-isolated-white-background_80590-13172_jpg-transformed.png" alt=""/>
+            </div>
+
+            <div class="flex flex-col justify-center items-center h-25">
+                <h1 class="font-semibold text-xl text-yellow-500">Calliandra Honey</h1>
+                <p class="text-center p-3"></p>
+                <a href="" class="font-semibold bg-yellow-500 text-white hover:bg-yellow-700 hover:text-white px-2 py-1 rounded pt-2 pb-2">Info</a>
+             </div>
+
+        </div>
+
+        </ul>
+
+    </section>
+
+<!-- All About Honey -->
+
+
+    <section class="flex h-auto w-auto p-4 items-center justify center bg-amber-500">
+        
+        <div class="h-32 w-full bg-yellow-500 text-center">
+            <h1 class="font-semibold text-x1 text-white">All about Honey</h1>
+            <p class="text-center p-5">Learn more about honey</p>
+            <a href="" class="font-semibold bg-amber-300 text-white hover:bg-amber-500 hover:text-white px-2 py-1 rounded">Visit</a>
+        </div>
+        
+    </section>
+
+
+<!-- About and Contact -->
+
+    <div class="bg-amber-300 px-10 h-12 pt-2 items-center">
+
+        <ul class="flex gap-4">
+            <a href="" class="font-semibold bg-amber-500 text-white hover:bg-amber-700 hover:text-white px-2 py-1 rounded">About</a>
+            <a href="" class="font-semibold bg-amber-500 text-white hover:bg-amber-700 hover:text-white px-2 py-1 rounded">Contact</a>
+        </ul>
+
+    </div>
+    
+
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[PageController::class, 'index']);
